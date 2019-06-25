5e55a706900257cfb51e9b50ebf3f1793469b9a2
Reza Mohseni <rezamohseni256@gmail.com>
2019-06-26 01:34:51 +0430

    Initial commit

---
+++ src/items/items.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Put, Delete, Body, Param} from '@nestjs/common';
+import {CreateItemDto as CreateItemDto} from './dto/create-item.dto'
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
+
+@Controller('items')
+export class ItemsController {
+    constructor(private readonly itemService: ItemsService) {}
+
+    @Get()
+    findAll(): Promise<Item[]> {
+        return this.itemService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id): Promise<Item> {
+        return this.itemService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() createItemDto: CreateItemDto): Promise<Item>{
+        return this.itemService.create(createItemDto);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id): Promise<Item> {
+        return this.itemService.delete(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id, @Body() updateItemDto: CreateItemDto): Promise<Item> {
+        return this.itemService.update(id, updateItemDto);
+    }
+}
